118a175977ec1ab70501f60b68bdb50a84500d22
Aydin Ramezani <aydinramzni@gmail.com>
2024-06-03 08:46:23 +0330

    Created sale.js

---
+++ scripts/models/sale.js
@@ -0,0 +1,62 @@
+const Sale = [
+  {
+    saleId: 0,
+    productId: '1a9f33bb-75d4-4bda-b9d0-3fb1f98e9b02',
+    quantity: 8,
+    timestamps: Date.now(),
+  },
+  {
+    saleId: 1,
+    productId: '3d84c635-cf49-4d8e-9069-2f4b432c0b5b',
+    quantity: 3,
+    timestamps: Date.now(),
+  },
+  {
+    saleId: 2,
+    productId: '4a55d46c-1c34-4bd7-bf6d-7f67a5f2a915',
+    quantity: 11,
+    timestamps: Date.now(),
+  },
+  {
+    saleId: 3,
+    productId: '7b36c68a-0f35-4e5e-bd6a-3a8f7f3c6957',
+    quantity: 2,
+    timestamps: Date.now(),
+  },
+  {
+    saleId: 4,
+    productId: '9d02b56d-2fa1-4e4a-94d5-647a23a6f5a7',
+    quantity: 7,
+    timestamps: Date.now(),
+  },
+  {
+    saleId: 5,
+    productId: 'b1f44c7a-41c6-4dcd-a6e4-d587a93d2a9a',
+    quantity: 1,
+    timestamps: Date.now(),
+  },
+  {
+    saleId: 6,
+    productId: 'd3e6f78a-65d3-4e6e-96c8-4f75e5c8f7e8',
+    quantity: 6,
+    timestamps: Date.now(),
+  },
+  {
+    saleId: 7,
+    productId: 'e7e3f896-0c95-4b9b-95d3-6b9c8f8c7e7e',
+    quantity: 10,
+    timestamps: Date.now(),
+  },
+  {
+    saleId: 8,
+    productId: 'f0e5d69b-2d89-4cdd-88d3-5f5c3c8f8e8f',
+    quantity: 5,
+    timestamps: Date.now(),
+  },
+  {
+    saleId: 9,
+    productId: 'a6c2e77d-4a97-4c2d-85d3-8e8e9f9e8e8e',
+    quantity: 4,
+    timestamps: Date.now(),
+  },
+];
